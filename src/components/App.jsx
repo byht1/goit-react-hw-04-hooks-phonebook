e4bf25770350703de426e4f1f33e99e291be40b8
byht1 <vitalikbyht1@gmail.com>
2022-07-09 22:58:37 +0300

    ujnjdj

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,93 +1,75 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Filter } from './Phonebook/Filter/Filter';
 import ContactForm from './Phonebook/ContactForm/ContactForm';
 import ContactList from './Phonebook/ContactList/ContactList';
 import { Section, Containet, H1, DivList } from './App.stiled';
 
 const L_KEY = 'contacts-list';
+const contactsList = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
 
-export default class App extends Component {
-  state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    fillet: '',
-  };
+export default function App() {
+  const [contacts, setContacts] = useState(
+    () => JSON.parse(window.localStorage.getItem(L_KEY)) ?? contactsList
+  );
+  const [fillet, setFillet] = useState('');
 
-  componentDidMount() {
-    const local = localStorage.getItem(L_KEY);
-    if (local !== null) {
-      this.setState({ contacts: JSON.parse(local) });
+  useEffect(() => {
+    localStorage.setItem(L_KEY, JSON.stringify(contacts));
+  }, [contacts]);
+
+  function contactsFillet() {
+    if (fillet === '') {
+      return false;
     }
+
+    return contacts.filter(x => x.name.toLowerCase().includes(fillet));
   }
 
-  componentDidUpdate(_, prevState) {
-    const nextState = this.state.contacts;
-    if (prevState.Component !== nextState) {
-      localStorage.setItem(L_KEY, JSON.stringify(nextState));
-    }
-  }
-
-  newContacts = newObj => {
-    if (this.state.contacts.some(x => x.name === newObj.name)) {
+  function newC(newObj) {
+    if (contacts.some(x => x.name === newObj.name)) {
       alert(`${newObj.name} is already is contacts`);
       return false;
     }
-    this.setState(prevState => ({
-      contacts: [...prevState.contacts, newObj],
-    }));
+    setContacts(prevState => [...prevState, newObj]);
     return true;
-  };
+  }
 
-  onCahangeFilter = event => {
-    this.setState({
-      fillet: event.target.value.toLowerCase(),
-    });
-  };
+  function onCahangeFilter(event) {
+    setFillet(event.target.value.toLowerCase());
+  }
 
-  fillet = () => {
-    if (this.state.fillet !== '') {
-      return this.state.contacts.filter(x =>
-        x.name.toLowerCase().includes(this.state.fillet)
-      );
-    }
-
-    return false;
-  };
-
-  deleteContacts = eve => {
-    const index = this.state.contacts.findIndex(x => x.id === eve.target.name);
+  function deleteContacts(event) {
+    const index = contacts.findIndex(x => x.id === event.target.name);
 
     // Перевірки на -1 не має так як це не можливо!!!! (сподіваюсь)
 
-    this.setState(this.state.contacts.splice(index, 1));
-  };
-
-  render() {
-    const { contacts } = this.state;
-    const fillter = this.fillet();
-    return (
-      <Section>
-        <Containet>
-          <div>
-            <H1>Phonebook</H1>
-            <ContactForm newContacts={this.newContacts} />
-          </div>
-
-          <DivList>
-            <h2>Contacts</h2>
-            <Filter onChange={this.onCahangeFilter} />
-            <ContactList
-              fillter={fillter}
-              contacts={contacts}
-              deleteContacts={this.deleteContacts}
-            />
-          </DivList>
-        </Containet>
-      </Section>
-    );
+    setContacts(prev => prev.filter(x => x.id !== event.target.name));
   }
+
+  const fillter = contactsFillet();
+  return (
+    <Section>
+      <Containet>
+        <div>
+          <H1>Phonebook</H1>
+          <ContactForm newC={newC} />
+        </div>
+
+        <DivList>
+          <h2>Contacts</h2>
+          <Filter onChange={onCahangeFilter} />
+          <ContactList
+            fillter={fillter}
+            contacts={contacts}
+            deleteContacts={deleteContacts}
+          />
+        </DivList>
+      </Containet>
+    </Section>
+  );
 }
--- src/components/Phonebook/ContactForm/ContactForm.jsx
+++ src/components/Phonebook/ContactForm/ContactForm.jsx
@@ -1,73 +1,70 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { nanoid } from 'nanoid';
 import PropTypes from 'prop-types';
 import { Span, Input, Button, Form } from './ContactForm.styled';
 
-export default class ContactForm extends Component {
-  state = {
-    name: '',
-    number: '',
-  };
+const INITIAL_STATE = {
+  name: '',
+  number: '',
+};
 
-  onChange = eve => {
-    this.setState({ [eve.target.name]: eve.target.value });
-  };
+export default function ContactForm({ newC }) {
+  const [{ name, number }, setState] = useState(INITIAL_STATE);
 
-  onSubmit = eve => {
+  function onChange(eve) {
+    const { name, value } = eve.target;
+    setState(prevState => ({ ...prevState, [name]: value }));
+  }
+
+  function onSubmit(eve) {
     eve.preventDefault();
     const newContacts = {
       id: nanoid(5),
-      name: this.state.name,
-      number: this.state.number,
+      name,
+      number,
     };
-    const isUnique = this.props.newContacts(newContacts);
+    const isUnique = newC(newContacts);
 
     if (isUnique) {
-      this.setState({
-        name: '',
-        number: '',
-      });
+      setState({ ...INITIAL_STATE });
     }
-  };
-
-  render() {
-    const { name, number } = this.state;
-    return (
-      <Form onSubmit={this.onSubmit}>
-        <label htmlFor="name">
-          <Span>Name</Span>
-          <Input
-            onChange={this.onChange}
-            type="text"
-            name="name"
-            value={name}
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            required
-            placeholder="Rosie Simpson"
-          />
-        </label>
-
-        <label htmlFor="number">
-          <Span>Number</Span>
-          <Input
-            onChange={this.onChange}
-            type="tel"
-            name="number"
-            value={number}
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-            required
-            placeholder="645-17-79"
-          />
-        </label>
-
-        <Button type="submit">Add contact</Button>
-      </Form>
-    );
   }
+
+  return (
+    <Form onSubmit={onSubmit}>
+      <label htmlFor="name">
+        <Span>Name</Span>
+        <Input
+          onChange={onChange}
+          type="text"
+          name="name"
+          value={name}
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+          placeholder="Rosie Simpson"
+        />
+      </label>
+
+      <label htmlFor="number">
+        <Span>Number</Span>
+        <Input
+          onChange={onChange}
+          type="tel"
+          name="number"
+          value={number}
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+          placeholder="645-17-79"
+        />
+      </label>
+
+      <Button type="submit">Add contact</Button>
+    </Form>
+  );
 }
 
 ContactForm.propTypes = {
-  newContacts: PropTypes.func.isRequired,
+  newC: PropTypes.func.isRequired,
 };
